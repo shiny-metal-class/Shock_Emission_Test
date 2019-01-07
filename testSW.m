59e6a80b895d1868a30beacf4b2512d4c3971392
shiny-metal-class <31256512+shiny-metal-class@users.noreply.github.com>
2019-01-07 17:41:58 -0600

    Edits
    Edits

--- testSW.m
+++ testSW.m
@@ -1,9 +1,9 @@
 clear all;
-X = ShockWave; X.LT = 20; X.L = 165; X.Us = 7.25; X.E_0 = 20000; 
-x = linspace(0,X.L+10); X.Alpha = 0.1; X.Beta = 0;
-t = linspace(0,50);
+X = ShockWave; X.LT = 0.5; X.L = 165; X.Us = 7.25; X.E_0 = 7000; 
+x = linspace(0,X.L+10,10000); X.Alpha = 0.01; X.Beta = 0;
+t = linspace(0,30);
 for j = 1:length(t)
-    for i = 1:length(x)
+    parfor i = 1:length(x)
         A(i,j) = X.Stream(x(i),t(j));
         E(i,j) = X.ABS(x(i),t(j));
     end
@@ -11,12 +11,11 @@ for j = 1:length(t)
     plot(x,E(:,j),x,A(:,j));ylim([0,20000]);
     label = sprintf('t = %g',t(j));
     legend(label);
-    pause(0.05);
+    pause(0.005);
 end
 
-for i = 1:length(t)
+parfor i = 1:length(t)
     I(i) = X.Int_En(t(i));
-    %I(i) = trapz(x,E(:,i));
 end
 figure(2); hold off;
 plot(t,I); legend(sprintf('%g',X.L/X.Us));
